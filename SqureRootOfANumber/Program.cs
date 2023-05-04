3dbbcd7fef951dd3018aef1d3aca997345d1449f
Rahulalvk <rahulal432@gmail.com>
2023-05-04 04:51:29 -0700

    Program Find the squreroot of a number

---
+++ SqureRootOfANumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqureRootOfANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqureRootOfANumber.DisplaySqureRoot();
+        }
+    }
+}
---
+++ SqureRootOfANumber/SqureRootOfANumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqureRootOfANumber
+{
+    internal class SqureRootOfANumber
+    {
+        public static void DisplaySqureRoot()
+        {
+            Console.Write("Enter a non-negative number: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double epsilon = 1e-15;
+            double t = c;
+
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+
+            Console.WriteLine("The square root of {0} is {1}", c, t);
+        }
+    }
+}
